d1ed3ddd5a3bbc1b353f6f6ece391f0294fa491d
Adrian Kastrau <akastrau@outlook.com>
2025-06-13 21:53:12 +0200

    Update kubeconfig.tf

--- kubeconfig.tf
+++ kubeconfig.tf
@@ -1,6 +1,6 @@
 data "remote_file" "kubeconfig" {
   conn {
-    host        = can(ipv6(local.first_control_plane_ip)) ? "[${local.first_control_plane_ip}]" : local.first_control_plane_ip
+    host        = can(regex("^[0-9a-fA-F:]+$", local.local.first_control_plane_ip)) ? "[${local.first_control_plane_ip}]" : local.first_control_plane_ip
     port        = var.ssh_port
     user        = "root"
     private_key = var.ssh_private_key
